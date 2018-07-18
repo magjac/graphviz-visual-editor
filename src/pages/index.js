76e325d796089234bdf760088010db0de9aca3bb
Magnus Jacobsson <Magnus.Jacobsson@netinsight.net>
2018-07-18 11:38:26 +0200

    Text editor to the left and graph to the right

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import withRoot from '../withRoot';
 import ButtonAppBar from '../ButtonAppBar';
@@ -32,11 +34,21 @@ class Index extends React.Component {
       <div className={classes.root}>
         <ButtonAppBar>
         </ButtonAppBar>
-        <TextEditor
-          dotSrc={this.state.dotSrc}
-          onTextChange={this.handleTextChange}
-        />
-        <Graph dotSrc={this.state.dotSrc} />
+        <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <TextEditor
+                dotSrc={this.state.dotSrc}
+                onTextChange={this.handleTextChange}
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <Graph dotSrc={this.state.dotSrc} />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
