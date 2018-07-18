7171231fb224853ef92d1ba6a7b95b30fe5e9936
Magnus Jacobsson <Magnus.Jacobsson@netinsight.net>
2018-07-18 11:00:49 +0200

    Removed the original example's components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import withRoot from '../withRoot';
 import ButtonAppBar from '../ButtonAppBar';
@@ -21,22 +14,9 @@ const styles = theme => ({
 
 class Index extends React.Component {
   state = {
-    open: false,
     dotSrc: `strict digraph {
   a -> b
 }`,
-  };
-
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  handleClick = () => {
-    this.setState({
-      open: true,
-    });
   };
 
   handleTextChange = (text) => {
@@ -47,32 +27,11 @@ class Index extends React.Component {
 
   render() {
     const { classes } = this.props;
-    const { open } = this.state;
 
     return (
       <div className={classes.root}>
         <ButtonAppBar>
         </ButtonAppBar>
-        <Dialog open={open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Typography variant="display1" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subheading" gutterBottom>
-          example project
-        </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
         <TextEditor
           dotSrc={this.state.dotSrc}
           onTextChange={this.handleTextChange}
