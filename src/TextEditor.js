cb786e6efb20faabb6a9f9d241df5e1243702f73
Magnus Jacobsson <Magnus.Jacobsson@netinsight.net>
2018-07-24 16:59:37 +0200

    Replaced simple text editor with Ace

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,46 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
-});
+import AceEditor from 'react-ace';
+import 'brace/mode/dot';
+import 'brace/theme/github';
 
 class TextEditor extends React.Component {
 
-  handleChange = () => event => {
-    this.props.onTextChange(event.target.value);
+  handleChange = (value, event) => {
+    this.props.onTextChange(value);
   };
 
   render() {
-    const { classes } = this.props;
-
     return (
-      <form className={classes.container} noValidate autoComplete="off">
-        <TextField
-          id="dotSrc"
-          multiline
-          fullWidth
+      <div>
+        <AceEditor
+          mode="dot"
+          theme="github"
+          onChange={this.handleChange}
+          name="UNIQUE_ID_OF_DIV"
           value={this.props.dotSrc}
-          onChange={this.handleChange()}
-          className={classes.textField}
-          margin="normal"
+          editorProps={{
+            $blockScrolling: true
+          }}
         />
-      </form>
+      </div>
     );
   }
 }
 
-TextEditor.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(TextEditor);
+export default TextEditor;
