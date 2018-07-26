0fefd9b508c2fab0f2b24b4d5e9e70f56354ea0c
Magnus Jacobsson <Magnus.Jacobsson@netinsight.net>
2018-07-26 11:45:21 +0200

    New primary and secondary color

--- src/withRoot.js
+++ src/withRoot.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 // A theme with custom primary and secondary color.
@@ -9,14 +7,10 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
+      main: "#99ccff",
     },
     secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
+      main: "#ffffcc",
     },
   },
 });
