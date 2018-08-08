f77f880a1c94f01c8835f08f04f446ab32b05c04
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2018-08-08 14:23:25 +0200

    Removed the non-operational arrow head panel
    It's unclear there should be such a panel att all. Perhaps only a node
    arrow type select in the format drawer.

--- src/DrawingPanels.js
+++ src/DrawingPanels.js
@@ -82,49 +82,6 @@ class DrawingPanels extends React.Component {
             )}
           </ExpansionPanelDetails>
         </ExpansionPanel>
-        <ExpansionPanel expanded={expanded === 'arrowHeadPanel'} onChange={this.handleChange('arrowHeadPanel')}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Edge</Typography>
-            <Typography className={classes.secondaryHeading}>
-              arrow heads
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails className={classes.columns}>
-            <div className={classes.column}>
-              normal
-            </div>
-            <div className={classes.column}>
-              dot
-            </div>
-            <div className={classes.column}>
-              box
-            </div>
-            <div className={classes.column}>
-              crow
-            </div>
-            <div className={classes.column}>
-              curve
-            </div>
-            <div className={classes.column}>
-              icurve
-            </div>
-            <div className={classes.column}>
-              diamond
-            </div>
-            <div className={classes.column}>
-              inv
-            </div>
-            <div className={classes.column}>
-              vee
-            </div>
-            <div className={classes.column}>
-              tee
-            </div>
-            <div className={classes.column}>
-              none
-            </div>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
       </div>
     );
   }
