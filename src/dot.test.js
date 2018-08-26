bfbf9678eae14b91482e6df324f1302643fd5d66
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2018-08-26 15:34:55 +0200

    Put the DotGraph.toString() tests in a test suite

--- src/dot.test.js
+++ src/dot.test.js
@@ -7,44 +7,48 @@ const WrapDot = props => {
   return <p>{dotGraph.toString()}</p>;
 };
 
-it('renders an empty graph', () => {
-  let dotSrc = 'digraph {}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
-});
+describe('dot.DotGraph.toString()', () => {
 
-it('renders a single node without attributes', () => {
-  let dotSrc = 'digraph {a}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
-});
+  it('renders an empty graph', () => {
+    let dotSrc = 'digraph {}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
 
-it('renders two nodes without attributes', () => {
-  let dotSrc = 'digraph {a b}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
-});
+  it('renders a single node without attributes', () => {
+    let dotSrc = 'digraph {a}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
 
-it('renders three nodes without attributes', () => {
-  let dotSrc = 'digraph {a b c}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
-});
+  it('renders two nodes without attributes', () => {
+    let dotSrc = 'digraph {a b}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
 
-it('renders an edge between two nodes in an undirected graph', () => {
-  let dotSrc = 'graph {a -- b}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
-});
+  it('renders three nodes without attributes', () => {
+    let dotSrc = 'digraph {a b c}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
 
-it('renders an edge between two nodes in a directed graph', () => {
-  let dotSrc = 'digraph {a -> b}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
-});
+  it('renders an edge between two nodes in an undirected graph', () => {
+    let dotSrc = 'graph {a -- b}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
 
-it('renders an edge between three nodes', () => {
-  let dotSrc = 'digraph {a -> b -> c}';
-  const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
-  expect(wrapper.find('p').text()).toEqual(dotSrc);
+  it('renders an edge between two nodes in a directed graph', () => {
+    let dotSrc = 'digraph {a -> b}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
+
+  it('renders an edge between three nodes', () => {
+    let dotSrc = 'digraph {a -> b -> c}';
+    const wrapper = shallow(<WrapDot dotSrc={dotSrc} />);
+    expect(wrapper.find('p').text()).toEqual(dotSrc);
+  });
+
 });
