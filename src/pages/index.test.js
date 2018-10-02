9a0c5348ee659ba526e4c39f9cffc569fda919d7
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2018-10-02 07:24:05 +0200

    Test: Added default DOT source test

--- src/pages/index.test.js
+++ src/pages/index.test.js
@@ -17,4 +17,13 @@ describe('<Index />', () => {
     const wrapper = mount(<Index />);
   });
 
+  it('state has default DOT source after mount', () => {
+    const wrapper = mount(<Index />);
+    const indexWrapper = wrapper.find('Index');
+    const index = indexWrapper.instance();
+    const expectedDotSrc = 'strict digraph {\n    a [shape="ellipse" style="filled" fillcolor="#1f77b4"]\n    b [shape="polygon" style="filled" fillcolor="#ff7f0e"]\n    a -> b [fillcolor="#a6cee3" color="#1f78b4"]\n}';
+    const actualDotSrc = index.state.dotSrc;
+    expect(actualDotSrc).toEqual(expectedDotSrc);
+  });
+
 });
