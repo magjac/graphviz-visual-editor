30fa855d26997170f9b89e81e9635860ec9337ce
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2019-10-16 11:08:55 +0200

    Add test of node insertion by middle mouse click

---
+++ cypress/integration/insert_node.spec.js
@@ -0,0 +1,37 @@
+describe('Insertion of nodes into the graph', function() {
+
+  it('Inserts a node when middle mouse button is clicked', function() {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('.ace_text-input').type('{ctrl}a{del}', {force: true});
+    cy.get('.ace_text-input').type('digraph {{}Alice -> Bob}', {force: true});
+
+    cy.get('#graph0 > #node1').should('exist');
+    cy.get('#graph0 > #node2').should('exist');
+    cy.get('#graph0 > #edge1').should('exist');
+
+    cy.get('#graph0 > #node1 > title').should('have.text', 'Alice');
+    cy.get('#graph0 > #node2 > title').should('have.text', 'Bob');
+    cy.get('#graph0 > #edge1 > title').should('have.text', 'Alice->Bob');
+
+    cy.get('#graph0 > .node').should('have.length', 2);
+    cy.get('#graph0 > .edge').should('have.length', 1);
+
+    cy.get('#graph0').trigger('mousedown', 'topLeft', {which: 2});
+    cy.get('#graph0').trigger('mouseup', 'topLeft', {which: 2});
+
+    cy.get('#graph0 > #node1').should('exist');
+    cy.get('#graph0 > #node2').should('exist');
+    cy.get('#graph0 > #node3').should('exist');
+    cy.get('#graph0 > #edge1').should('exist');
+
+    cy.get('#graph0 > #node1 > title').should('have.text', 'Alice');
+    cy.get('#graph0 > #node2 > title').should('have.text', 'Bob');
+    cy.get('#graph0 > #node3 > title').should('have.text', 'n2');
+    cy.get('#graph0 > #edge1 > title').should('have.text', 'Alice->Bob');
+
+    cy.get('#graph0 > .node').should('have.length', 3);
+    cy.get('#graph0 > .edge').should('have.length', 1);
+  })
+
+})
