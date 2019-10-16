bfb2811e38ff0101be6f78623ad4a0b644584aa3
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2019-10-16 09:00:59 +0200

    Add test of single edge selection and deletion

--- cypress/integration/select_delete.spec.js
+++ cypress/integration/select_delete.spec.js
@@ -30,4 +30,35 @@ describe('Selection and deletion in graph', function() {
     cy.get('#graph0 > .edge').should('have.length', 0);
   })
 
+  it('Selects an edge and deletes it', function() {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('.ace_text-input').type('{ctrl}a{del}', {force: true});
+    cy.get('.ace_text-input').type('digraph {{}Alice -> Bob}', {force: true});
+
+    cy.get('#graph0 > #node1').should('exist');
+    cy.get('#graph0 > #node2').should('exist');
+    cy.get('#graph0 > #edge1').should('exist');
+
+    cy.get('#graph0 > #node1 > title').should('have.text', 'Alice');
+    cy.get('#graph0 > #node2 > title').should('have.text', 'Bob');
+    cy.get('#graph0 > #edge1 > title').should('have.text', 'Alice->Bob');
+
+    cy.get('#graph0 > .node').should('have.length', 2);
+    cy.get('#graph0 > .edge').should('have.length', 1);
+
+    cy.get('#graph0 > #edge1').click();
+    cy.get('body').type('{del}');
+
+    cy.get('#graph0 > #node1').should('exist');
+    cy.get('#graph0 > #node2').should('exist');
+    cy.get('#graph0 > #edge1').should('not.exist');
+
+    cy.get('#graph0 > #node1 > title').should('have.text', 'Alice');
+    cy.get('#graph0 > #node2 > title').should('have.text', 'Bob');
+
+    cy.get('#graph0 > .node').should('have.length', 2);
+    cy.get('#graph0 > .edge').should('have.length', 0);
+  })
+
 })
