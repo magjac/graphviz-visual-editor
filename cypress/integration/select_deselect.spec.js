45dd8f58503f5636572093d709db0aad8d33440f
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2019-10-16 09:43:21 +0200

    Add test of change of node selection

--- cypress/integration/select_deselect.spec.js
+++ cypress/integration/select_deselect.spec.js
@@ -25,4 +25,35 @@ describe('Selection and deselection in graph', function() {
     cy.get('#graph0 > #edge1 > rect').should('not.exist');
   })
 
+  it('Deselects a selected node when another node is clicked and selects that node instead', function() {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('.ace_text-input').type('{ctrl}a{del}', {force: true});
+    cy.get('.ace_text-input').type('digraph {{}Alice -> Bob}', {force: true});
+
+    cy.get('#graph0 > #node1').should('exist');
+    cy.get('#graph0 > #node2').should('exist');
+    cy.get('#graph0 > #edge1').should('exist');
+
+    cy.get('#graph0 > #node1 > title').should('have.text', 'Alice');
+    cy.get('#graph0 > #node2 > title').should('have.text', 'Bob');
+    cy.get('#graph0 > #edge1 > title').should('have.text', 'Alice->Bob');
+
+    cy.get('#graph0 > #node1 > rect').should('not.exist');
+    cy.get('#graph0 > #node2 > rect').should('not.exist');
+    cy.get('#graph0 > #edge1 > rect').should('not.exist');
+
+    cy.get('#graph0 > #node1').click();
+
+    cy.get('#graph0 > #node1 > rect').should('exist');
+    cy.get('#graph0 > #node2 > rect').should('not.exist');
+    cy.get('#graph0 > #edge1 > rect').should('not.exist');
+
+    cy.get('#graph0 > #node2').click();
+
+    cy.get('#graph0 > #node1 > rect').should('not.exist');
+    cy.get('#graph0 > #node2 > rect').should('exist');
+    cy.get('#graph0 > #edge1 > rect').should('not.exist');
+  })
+
 })
