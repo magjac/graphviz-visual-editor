7b029a5a21690aef0e675edbc415f73e5a9b8029
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-01-05 12:01:36 +0100

    Add test of node insertion by copy-and-paste

--- cypress/integration/insert_node.spec.js
+++ cypress/integration/insert_node.spec.js
@@ -150,4 +150,38 @@ describe('Insertion of nodes into the graph', function() {
     cy.edges().should('have.length', 1);
   })
 
+  it('Inserts a node by copy-and-paste another node', function() {
+    cy.startApplication();
+    cy.clearAndRender('digraph {Alice -> Bob}');
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+
+    cy.nodes().should('have.length', 2);
+    cy.edges().should('have.length', 1);
+
+    cy.node(1).click();
+    cy.get('body').type('{ctrl}c');
+    cy.get('body').type('{ctrl}v');
+    cy.waitForTransition();
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.node(3).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.node(3).shouldHaveName('n2');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+
+    cy.nodes().should('have.length', 3);
+    cy.edges().should('have.length', 1);
+  })
+
 })
