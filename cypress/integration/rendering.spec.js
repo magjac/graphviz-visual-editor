5a242131c1662bc83ed6fc8cd0ecf9acb44830da
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-01-27 23:03:10 +0100

    Add test of rendering with path tweening disabled

--- cypress/integration/rendering.spec.js
+++ cypress/integration/rendering.spec.js
@@ -328,4 +328,48 @@ describe('Basic rendering from DOT source', function() {
 
   })
 
+  it('Renders a new graph without path tweening when disabled in settings', function() {
+    cy.startApplicationWithDotSource('digraph {Alice -> Bob}');
+
+    cy.settingsButton().click();
+    cy.pathTweenSwitch().click();
+    cy.shapeTweenSwitch().click();
+    cy.get('body').type('{esc}', { release: false });
+
+    cy.canvasGraph().then(graph0 => {
+      cy.wrap(graph0).findEdge(1)
+        .should('exist')
+        .shouldHaveName('Alice->Bob')
+        .shouldHaveShape('path')
+        .find('> path').should('have.attr', 'd', 'M31.9486,-71.8314C31.9486,-64.131 31.9486,-54.9743 31.9486,-46.4166');
+    });
+
+    cy.typeDotSource('{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow}{leftArrow} rankdir=LR ', {force: true});
+
+    cy.waitForBusy();
+
+    cy.canvasGraph().then(graph0 => {
+      cy.wrap(graph0).findEdge(1)
+        .should('exist')
+        .shouldHaveName('Alice->Bob')
+        .shouldHaveShape('path')
+        .find('> path').first()
+        .should('not.have.attr', 'd', 'M31.9486,-71.8314C31.9486,-64.131 31.9486,-54.9743 31.9486,-46.4166')
+        .then(path => {
+          expect(path.attr('d').split(',').length).to.equal(5);
+        });
+    });
+
+    cy.waitForNotBusy();
+
+    cy.canvasGraph().then(graph0 => {
+      cy.wrap(graph0).findEdge(1)
+        .should('exist')
+        .shouldHaveName('Alice->Bob')
+        .shouldHaveShape('path')
+        .find('> path').should('have.attr', 'd', 'M64.1391,-18C72.2457,-18 81.0193,-18 89.3677,-18');
+    });
+
+  })
+
 })
