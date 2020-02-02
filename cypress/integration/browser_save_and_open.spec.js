174e913f64e56c09e9792692ed0f9c921b7b7e37
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-02 17:39:55 +0100

    Add test of load of saved graph from browser local storage through double-click

--- cypress/integration/browser_save_and_open.spec.js
+++ cypress/integration/browser_save_and_open.spec.js
@@ -151,6 +151,95 @@ describe('Browser save and open', function() {
     cy.edges().should('have.length', 1);
   })
 
+  it('The graph is loaded from a saved graph in browser local storage when double-clicked', function() {
+    cy.startApplication();
+    cy.clearAndRenderDotSource('digraph {Alice -> Bob}');
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+
+    cy.nodes().should('have.length', 2);
+    cy.edges().should('have.length', 1);
+
+    cy.saveAsButton().click();
+
+    cy.saveToBrowserDialog().should('exist');
+
+    cy.saveToBrowserNameInput().type('My graph');
+    cy.saveToBrowserSaveButton().click()
+
+    cy.saveToBrowserDialog().should('not.exist');
+
+    cy.openButton().click();
+
+    cy.openFromBrowserDialog().should('exist');
+
+    cy.savedGraphs().should('have.length', 2);
+    cy.savedGraphName(1).should('have.text', 'My graph');
+    cy.savedGraphDotSource(1).should('have.text', 'digraph {Alice -> Bob}');
+    cy.savedGraphTime(1).should('have.text', 'a few seconds ago');
+    cy.savedGraphPreview(1).should('have.text', '\n\n%0\n\n\n\nAlice\n\nAlice\n\n\n\nBob\n\nBob\n\n\n\nAlice->Bob\n\n\n\n\n');
+
+    cy.openGraphCancelButton().click();
+
+    cy.openFromBrowserDialog().should('not.exist');
+
+    cy.clearAndRenderDotSource('digraph {Charlie -> Daphne -> Ernie}');
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.node(3).should('exist');
+    cy.edge(1).should('exist');
+    cy.edge(2).should('exist');
+
+    cy.node(1).shouldHaveName('Charlie');
+    cy.node(2).shouldHaveName('Daphne');
+    cy.node(3).shouldHaveName('Ernie');
+    cy.edge(1).shouldHaveName('Charlie->Daphne');
+    cy.edge(2).shouldHaveName('Daphne->Ernie');
+
+    cy.nodes().should('have.length', 3);
+    cy.edges().should('have.length', 2);
+
+    cy.openButton().click();
+
+    cy.openFromBrowserDialog().should('exist');
+
+    cy.savedGraphs().should('have.length', 2);
+
+    cy.savedGraphName(0).should('have.text', 'My graph');
+    cy.savedGraphDotSource(0).should('have.text', 'digraph {Charlie -> Daphne -> Ernie}');
+    cy.savedGraphTime(0).should('have.text', 'a few seconds ago');
+    cy.savedGraphPreview(0).should('have.text', '\n\n%0\n\n\n\nCharlie\n\nCharlie\n\n\n\nDaphne\n\nDaphne\n\n\n\nCharlie->Daphne\n\n\n\n\n\nErnie\n\nErnie\n\n\n\nDaphne->Ernie\n\n\n\n\n');
+
+    cy.savedGraphName(1).should('have.text', 'Untitled Graph');
+    cy.savedGraphDotSource(1).should('have.text', 'digraph {Alice -> Bob}');
+    cy.savedGraphTime(1).should('have.text', 'a few seconds ago');
+    cy.savedGraphPreview(1).should('have.text', '\n\n%0\n\n\n\nAlice\n\nAlice\n\n\n\nBob\n\nBob\n\n\n\nAlice->Bob\n\n\n\n\n');
+
+    cy.savedGraphName(1).trigger('dblclick');
+
+    cy.openFromBrowserDialog().should('not.exist');
+
+    cy.waitForTransition();
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+
+    cy.nodes().should('have.length', 2);
+    cy.edges().should('have.length', 1);
+  })
+
   it('The open from browser dialog allows sorting the view of stored graphs.', function() {
     cy.startApplication();
     cy.clearAndRenderDotSource('digraph {Alice -> Bob}');
