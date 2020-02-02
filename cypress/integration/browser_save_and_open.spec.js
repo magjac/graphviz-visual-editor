dc81bf0e4c16e0569b6ad5b01b2e3613d30d233b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-02-02 19:13:52 +0100

    Add test of retrieval of graphs from browser local storage

--- cypress/integration/browser_save_and_open.spec.js
+++ cypress/integration/browser_save_and_open.spec.js
@@ -26,6 +26,54 @@ describe('Browser save and open', function() {
     cy.savedGraphPreview(0).should('have.text', '\n\n%0\n\n\n\nAlice\n\nAlice\n\n\n\nBob\n\nBob\n\n\n\nAlice->Bob\n\n\n\n\n');
   })
 
+  it('Graphs stored in browser local storage are retrieved when application starts', function() {
+    localStorage.setItem('projects', '{"Untitled Graph":{"dotSrc":"digraph {Alice -> Bob}","dotSrcLastChangeTime":' + (Date.now() - 60 * 1000) + ',"svg":"<svg width=\\"936\\" height=\\"333\\" viewBox=\\"0.00 0.00 71.90 116.00\\" xmlns=\\"http://www.w3.org/2000/svg\\" xmlns:xlink=\\"http://www.w3.org/1999/xlink\\">\\n<g id=\\"graph0\\" class=\\"graph\\" transform=\\"translate(4.001399040222168, 112)\\">\\n<title>%0</title>\\n<polygon fill=\\"#ffffff\\" stroke=\\"transparent\\" points=\\"-4,4 -4,-112 67.8972,-112 67.8972,4 -4,4\\"/>\\n<!-- Alice -->\\n<g id=\\"node1\\" class=\\"node\\">\\n<title>Alice</title>\\n<ellipse fill=\\"none\\" stroke=\\"#000000\\" cx=\\"31.9486\\" cy=\\"-90\\" rx=\\"31.8973\\" ry=\\"18\\"/>\\n<text text-anchor=\\"middle\\" x=\\"31.9486\\" y=\\"-85.8\\" font-family=\\"Times,serif\\" font-size=\\"14.00\\" fill=\\"#000000\\">Alice</text>\\n</g>\\n<!-- bob -->\\n<g id=\\"node2\\" class=\\"node\\">\\n<title>Bob</title>\\n<ellipse fill=\\"none\\" stroke=\\"#000000\\" cx=\\"31.9486\\" cy=\\"-18\\" rx=\\"27.268\\" ry=\\"18\\"/>\\n<text text-anchor=\\"middle\\" x=\\"31.9486\\" y=\\"-13.8\\" font-family=\\"Times,serif\\" font-size=\\"14.00\\" fill=\\"#000000\\">Bob</text>\\n</g>\\n<!-- Alice&#45;&gt;bob -->\\n<g id=\\"edge1\\" class=\\"edge\\">\\n<title>Alice-&gt;Bob</title>\\n<path fill=\\"none\\" stroke=\\"#000000\\" d=\\"M31.9486,-71.8314C31.9486,-64.131 31.9486,-54.9743 31.9486,-46.4166\\"/>\\n<polygon fill=\\"#000000\\" stroke=\\"#000000\\" points=\\"35.4487,-46.4132 31.9486,-36.4133 28.4487,-46.4133 35.4487,-46.4132\\"/>\\n</g>\\n</g>\\n</svg>"}}');
+    localStorage.setItem('name', 'My graph');
+    localStorage.setItem('dotSrc', 'digraph {Alice -> Bob -> Charlie}');
+    localStorage.setItem('dotSrcLastChangeTime', Date.now().toString());
+    localStorage.setItem('svg', '<svg width=\\"936\\" height=\\"333\\" viewBox=\\"0.00 0.00 71.90 116.00\\" xmlns=\\"http://www.w3.org/2000/svg\\" xmlns:xlink=\\"http://www.w3.org/1999/xlink\\">\\n<g id=\\"graph0\\" class=\\"graph\\" transform=\\"translate(4.001399040222168, 112)\\">\\n<title>%0</title>\\n<polygon fill=\\"#ffffff\\" stroke=\\"transparent\\" points=\\"-4,4 -4,-112 67.8972,-112 67.8972,4 -4,4\\"/>\\n<!-- Alice -->\\n<g id=\\"node1\\" class=\\"node\\">\\n<title>Alice</title>\\n<ellipse fill=\\"none\\" stroke=\\"#000000\\" cx=\\"31.9486\\" cy=\\"-90\\" rx=\\"31.8973\\" ry=\\"18\\"/>\\n<text text-anchor=\\"middle\\" x=\\"31.9486\\" y=\\"-85.8\\" font-family=\\"Times,serif\\" font-size=\\"14.00\\" fill=\\"#000000\\">Alice</text>\\n</g>\\n<!-- bob -->\\n<g id=\\"node2\\" class=\\"node\\">\\n<title>Bob</title>\\n<ellipse fill=\\"none\\" stroke=\\"#000000\\" cx=\\"31.9486\\" cy=\\"-18\\" rx=\\"27.268\\" ry=\\"18\\"/>\\n<text text-anchor=\\"middle\\" x=\\"31.9486\\" y=\\"-13.8\\" font-family=\\"Times,serif\\" font-size=\\"14.00\\" fill=\\"#000000\\">Bob</text>\\n</g>\\n<!-- Alice&#45;&gt;bob -->\\n<g id=\\"edge1\\" class=\\"edge\\">\\n<title>Alice-&gt;Bob</title>\\n<path fill=\\"none\\" stroke=\\"#000000\\" d=\\"M31.9486,-71.8314C31.9486,-64.131 31.9486,-54.9743 31.9486,-46.4166\\"/>\\n<polygon fill=\\"#000000\\" stroke=\\"#000000\\" points=\\"35.4487,-46.4132 31.9486,-36.4133 28.4487,-46.4133 35.4487,-46.4132\\"/>\\n</g>\\n</g>\\n</svg>');
+
+    cy.startApplication();
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.node(3).should('exist');
+    cy.edge(1).should('exist');
+    cy.edge(2).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.node(3).shouldHaveName('Charlie');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+    cy.edge(2).shouldHaveName('Bob->Charlie');
+
+    cy.nodes().should('have.length', 3);
+    cy.edges().should('have.length', 2);
+
+    cy.openButton().click();
+    cy.openFromBrowserDialog()
+      .should('exist')
+      .savedGraphs().then(savedGraphs => {
+        cy.wrap(savedGraphs)
+          .should('have.length', 2)
+          .savedGraph(0).then(savedGraph => {
+            cy.wrap(savedGraph).savedGraphName().should('have.text', 'My graph');
+            cy.wrap(savedGraph).savedGraphDotSource().should('have.text', 'digraph {Alice -> Bob -> Charlie}');
+            cy.wrap(savedGraph).savedGraphTime().should('have.text', 'a few seconds ago');
+            cy.wrap(savedGraph).savedGraphPreview().should('have.text', '\n\n%0\n\n\n\nAlice\n\nAlice\n\n\n\nBob\n\nBob\n\n\n\nAlice->Bob\n\n\n\n\n\nCharlie\n\nCharlie\n\n\n\nBob->Charlie\n\n\n\n\n');
+            return cy.wrap(savedGraphs);
+          })
+          .savedGraph(1).then(savedGraph => {
+            cy.wrap(savedGraph).savedGraphName().should('have.text', 'Untitled Graph');
+            cy.wrap(savedGraph).savedGraphDotSource().should('have.text', 'digraph {Alice -> Bob}');
+            cy.wrap(savedGraph).savedGraphTime().should('have.text', 'a minute ago');
+            cy.wrap(savedGraph).savedGraphPreview().should('have.text', '\n\n%0\n\n\n\nAlice\n\nAlice\n\n\n\nBob\n\nBob\n\n\n\nAlice->Bob\n\n\n\n\n');
+          });
+        cy.openGraphCancelButton().click();
+      });
+
+  })
+
   it('The graph is saved to browser local storage when the save as button is clicked', function() {
     cy.startApplication();
     cy.clearAndRenderDotSource('digraph {Alice -> Bob}');
