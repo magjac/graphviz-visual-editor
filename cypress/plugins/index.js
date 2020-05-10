9041442bb01b350603edead9df773a6d474ca53c
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-05-10 08:59:22 +0200

    Adapt to @cypress/code-coverage v3
    See https://github.com/cypress-io/code-coverage#v2-to-v3

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,5 +14,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config)
+
+  return config;
 }
