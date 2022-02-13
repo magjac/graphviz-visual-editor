bb9d2c224d470db88cba5c808b62d253a2b4d04f
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-02-13 14:07:02 +0100

    add bin/generate-versions.py

---
+++ bin/generate-versions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""generator of versions JSON file from a CHANGELOG.md file"""
+
+import json
+import re
+import sys
+from typing import List
+
+def main(args: List[str]) -> int: # pylint: disable=missing-function-docstring
+
+  with open(args[1]) as fp:
+    versions = {}
+    for  line in fp:
+      mo = re.match("## \[([0-9][^\]]*)] [-–] (.*)$", line)
+      if mo:
+        version = mo.group(1)
+        release_date = mo.group(2)
+        versions[version] = {"release_date": release_date}
+
+  print(json.dumps(versions, indent=4))
+
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv))
