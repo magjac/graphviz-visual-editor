aeac5170fa452064b22fb87d8951e5037d9b9241
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-11-25 15:54:52 +0100

    add check of inserted node after disabling the node color attribute
    The immediate reason for doing this is to avoid flakiness in this test
    suite, that seems to have been caused by an ongoing transition when a
    test case ends. Since this is accomplished by waiting until the
    transition ends, we might as well check that the node is inserted in
    the graph as expected.
    
    No flakiness has yet been observed for this particular test, but it is
    assumed to exist, since the behavior is more or less the same as the
    tests that were fixed in the previous two commits. This potential
    flakiness is believed to have manifested it iself as:
    
      1) Insertion of nodes into the graph
           Default node color is seleced from the color picker in the node format drawer:
    
          Timed out retrying
          + expected - actual
    
          -'Alice'
          +'n0'

--- cypress/integration/insert_node.spec.js
+++ cypress/integration/insert_node.spec.js
@@ -813,6 +813,8 @@ describe('Insertion of nodes into the graph', function() {
     cy.nodes().should('have.length', 3);
     cy.edges().should('have.length', 1);
 
+    // now disable color and check that no color is used
+
     cy.toolbarButton('Node format').click();
     cy.colorSwitch().click();
     cy.toolbarButton('Node format').click();
@@ -822,6 +824,27 @@ describe('Insertion of nodes into the graph', function() {
 
     cy.textEditorVisibleLines().should('have.text', 'digraph {Alice -> Bob    n2 [ color=""]    n3}');
 
+    cy.waitForTransition();
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.node(3).should('exist');
+    cy.node(4).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.node(3).shouldHaveName('n2');
+    cy.node(4).shouldHaveName('n3');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+
+    cy.node(1).find('ellipse').should('not.have.attr', 'stroke-dasharray');
+    cy.node(2).find('ellipse').should('not.have.attr', 'stroke-dasharray');
+    cy.node(3).find('ellipse').should('not.have.attr', 'stroke-dasharray');
+    cy.node(4).find('ellipse').should('not.have.attr', 'stroke-dasharray');
+
+    cy.nodes().should('have.length', 4);
+    cy.edges().should('have.length', 1);
   })
 
   it('Default node color is seleced from the color picker in the node format drawer', function() {
