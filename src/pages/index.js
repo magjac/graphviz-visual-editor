89f6e7554ade5297e7f6dac68f341888431cdd2d
Magnus Jacobsson <mjacobss@qti.qualcomm.com>
2023-11-28 18:51:33 +0100

    Change to start without fullscreen

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ class Index extends React.Component {
       replaceName: '',
       exportAsUrlDialogIsOpen: false,
       exportAsSvgDialogIsOpen: false,
-      fullscreen: true,
+      fullscreen: false,
       insertPanelsAreOpen: (localStorage.getItem('insertPanelsAreOpen') || 'false') === 'true',
       nodeFormatDrawerIsOpen: (localStorage.getItem('nodeFormatDrawerIsOpen') || 'false') === 'true',
       edgeFormatDrawerIsOpen: (localStorage.getItem('edgeFormatDrawerIsOpen') || 'false') === 'true',
