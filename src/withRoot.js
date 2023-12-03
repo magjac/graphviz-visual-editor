a4da326c173e4fd9afae2a88bfcbf3830a476d3a
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-03 16:14:11 +0100

    explicitly set 'secondary' color for MuiCheckbox
    'primary' has been made the default in Material UI v5.

--- src/withRoot.js
+++ src/withRoot.js
@@ -14,6 +14,11 @@ const theme = createTheme({
     },
   },
   components: {
+    MuiCheckbox: {
+      defaultProps: {
+        color: "secondary",
+      },
+    },
     MuiSwitch: {
       defaultProps: {
         color: "secondary",
