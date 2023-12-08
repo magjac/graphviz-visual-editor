e41af22283629cd4f19a83a7ecef5721229f081d
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-08 19:40:55 +0100

    port generate-graphviz-version.js to ES modules and @hpcc-js/wasm 2.14.1

--- bin/generate-graphviz-version.js
+++ bin/generate-graphviz-version.js
@@ -1,26 +1,9 @@
 #! /usr/bin/env node
 
-var hpccWasm = require("@hpcc-js/wasm/dist");
-var fs_promises = require("fs").promises;
+import { Graphviz } from "@hpcc-js/wasm/graphviz";
 
-global.fetch = function (filename) {
-  return fs_promises.open(filename, 'r').then((filehandle) => {
-    return filehandle.readFile().then(data => {
-      return {
-        ok: true,
-        arrayBuffer: () => data,
-      };
-    });
-  });
-}
+const graphviz = await Graphviz.load();
+const graphvizVersion = graphviz.version();
 
-global.document = {
-  "currentScript": {
-    src: './node_modules/@hpcc-js/wasm/dist/index.js'
-  }
-};
-
-hpccWasm.graphvizVersion().then(graphvizVersion => {
-  console.log(`const graphvizVersion = "${graphvizVersion}";`);
-  console.log('export {graphvizVersion};');
-});
+console.log(`const graphvizVersion = "${graphvizVersion}";`);
+console.log('export {graphvizVersion};');
