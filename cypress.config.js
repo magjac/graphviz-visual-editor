36cd0891dcf74ca740e3cad7b8c9418209028c1a
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-09 10:49:19 +0100

    port cypress.config.js to ESM

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,10 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   projectId: '8wm34o',
   defaultCommandTimeout: 10000,
   video: true,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     excludeSpecPattern: '*~',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
