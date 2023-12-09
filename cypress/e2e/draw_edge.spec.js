f17dc877169597502e81652eff9b00f513d768f1
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-09 11:14:06 +0100

    adapt draw_edge.spec.js to Graphviz "transparent" to "none" change and enable all tests again
    See https://gitlab.com/graphviz/graphviz/-/merge_requests/2472.

--- cypress/e2e/draw_edge.spec.js
+++ cypress/e2e/draw_edge.spec.js
@@ -124,7 +124,6 @@ describe('Draw edges in the graph', function() {
     cy.edges().should('have.length', 2);
   })
 
-  /*
   it('Default edge style is seleced from one of the styles in the edge format drawer', function() {
     cy.startCleanApplication();
 
@@ -217,7 +216,7 @@ describe('Draw edges in the graph', function() {
             cy.wrap(edge).find('polygon').should('not.have.attr', 'stroke-dasharray');
             cy.wrap(edge).find('polygon').should('have.attr', 'fill', 'black');
             cy.wrap(edge).find('polygon').should('not.have.attr', 'stroke-width');
-            cy.wrap(edge).find('polygon').eq(0).should('have.attr', 'stroke', 'transparent');
+            cy.wrap(edge).find('polygon').eq(0).should('have.attr', 'stroke', 'none');
             cy.wrap(edge).find('polygon').eq(1).should('have.attr', 'stroke', 'black');
             break;
           }
@@ -228,9 +227,7 @@ describe('Draw edges in the graph', function() {
       cy.style(style).click();
     });
   })
-  */
 
-  /*
   it('Default edge style is seleced from multiple styles in the edge format drawer', function() {
     cy.startCleanApplication();
 
@@ -323,7 +320,7 @@ describe('Draw edges in the graph', function() {
             cy.wrap(edge).find('polygon').should('not.have.attr', 'stroke-dasharray');
             cy.wrap(edge).find('polygon').should('have.attr', 'fill', 'black');
             cy.wrap(edge).find('polygon').should('not.have', 'stroke-width');
-            cy.wrap(edge).find('polygon').eq(0).should('have.attr', 'stroke', 'transparent');
+            cy.wrap(edge).find('polygon').eq(0).should('have.attr', 'stroke', 'none');
             cy.wrap(edge).find('polygon').eq(1).should('have.attr', 'stroke', 'black');
             break;
           }
@@ -334,7 +331,6 @@ describe('Draw edges in the graph', function() {
       cy.edges().should('have.length', numberOfVisibleEdges);
     });
   })
-  */
 
   it('Default edge styles are deseleced in the edge format drawer', function() {
     cy.startCleanApplication();
@@ -441,7 +437,7 @@ describe('Draw edges in the graph', function() {
             cy.wrap(edge).find('polygon').should('not.have.attr', 'stroke-dasharray');
             cy.wrap(edge).find('polygon').should('have.attr', 'fill', 'black');
             cy.wrap(edge).find('polygon').should('not.have', 'stroke-width');
-            cy.wrap(edge).find('polygon').eq(0).should('have.attr', 'stroke', 'transparent');
+            cy.wrap(edge).find('polygon').eq(0).should('have.attr', 'stroke', 'none');
             cy.wrap(edge).find('polygon').eq(1).should('have.attr', 'stroke', 'black');
             break;
           }
@@ -453,7 +449,6 @@ describe('Draw edges in the graph', function() {
     });
   })
 
-  /*
   it('Default edge color is seleced from the color picker in the edge format drawer', function() {
     cy.startCleanApplication();
 
@@ -582,7 +577,7 @@ describe('Draw edges in the graph', function() {
       let expectedStrokeColor;
       let expectedStrokeOpacity;
       if (positionName == 'left') {
-        expectedStrokeColor = 'transparent';
+        expectedStrokeColor = 'none';
         expectedStrokeOpacity = null;
       } else {
         const {x, y} = positions['topRight'];
@@ -608,7 +603,7 @@ describe('Draw edges in the graph', function() {
         expect(path).to.have.attr('fill', 'none');
         expect(path).to.not.have.attr('fill-opacity');
         const actualStrokeColor = path.attr('stroke').replace('#', '');
-        if (expectedStrokeColor == 'transparent') {
+        if (expectedStrokeColor == 'none') {
           expect(actualStrokeColor).to.eq(expectedStrokeColor);
         } else {
           checkColor(actualStrokeColor, expectedStrokeColor, colorTolerance);
@@ -626,9 +621,7 @@ describe('Draw edges in the graph', function() {
     cy.formatDrawerCloseButton().click()
     cy.formatDrawer().should('not.exist');
   })
-  */
 
-  /*
   it('Default edge fillcolor is seleced from the fillcolor picker in the edge format drawer', function() {
     cy.startCleanApplication();
 
@@ -754,7 +747,7 @@ describe('Draw edges in the graph', function() {
       let expectedFillColor;
       let expectedFillOpacity;
       if (positionName == 'left') {
-        expectedFillColor = 'transparent';
+        expectedFillColor = 'none';
         expectedFillOpacity = null;
       } else {
         const {x, y} = positions['topRight'];
@@ -779,7 +772,7 @@ describe('Draw edges in the graph', function() {
         expect(polygon).to.have.attr('fill');
         expect(polygon).to.have.attr('stroke', 'black');
         const actualFillColor = polygon.attr('fill').replace('#', '');
-        if (expectedFillColor == 'transparent') {
+        if (expectedFillColor == 'none') {
           expect(actualFillColor).to.eq(expectedFillColor);
         } else {
           checkColor(actualFillColor, expectedFillColor, colorTolerance);
@@ -797,6 +790,5 @@ describe('Draw edges in the graph', function() {
     cy.formatDrawerCloseButton().click()
     cy.formatDrawer().should('not.exist');
   })
-  */
 
 })
