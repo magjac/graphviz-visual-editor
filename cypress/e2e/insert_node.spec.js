d553c82570d69b276f023e7db57cbbb79029a79b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-09 11:40:38 +0100

    adapt insert_node.spec.js to Graphviz transparent to none change and enable all tests again
    See https://gitlab.com/graphviz/graphviz/-/merge_requests/2472.

--- cypress/e2e/insert_node.spec.js
+++ cypress/e2e/insert_node.spec.js
@@ -846,7 +846,7 @@ describe('Insertion of nodes into the graph', function() {
     cy.edges().should('have.length', 1);
   })
 
-  it.skip('Default node color is seleced from the color picker in the node format drawer', function() {
+  it('Default node color is seleced from the color picker in the node format drawer', function() {
     localStorage.setItem('engine', 'circo');
     cy.startCleanApplication();
     cy.settingsButton().click();
@@ -967,7 +967,7 @@ describe('Insertion of nodes into the graph', function() {
       let expectedStrokeColor;
       let expectedStrokeOpacity;
       if (positionName == 'left') {
-        expectedStrokeColor = 'transparent';
+        expectedStrokeColor = 'none';
         expectedStrokeOpacity = null;
       } else {
         const {x, y} = positions['topRight'];
@@ -993,7 +993,7 @@ describe('Insertion of nodes into the graph', function() {
         expect(ellipse).to.have.attr('fill', 'none');
         expect(ellipse).to.not.have.attr('fill-opacity');
         const actualStrokeColor = ellipse.attr('stroke').replace('#', '');
-        if (expectedStrokeColor == 'transparent') {
+        if (expectedStrokeColor == 'none') {
           expect(actualStrokeColor).to.eq(expectedStrokeColor);
         } else {
           checkColor(actualStrokeColor, expectedStrokeColor, colorTolerance);
@@ -1089,7 +1089,7 @@ describe('Insertion of nodes into the graph', function() {
     cy.edges().should('have.length', 1);
   })
 
-  it.skip('Default node fillcolor is seleced from the fillcolor picker in the node format drawer', function() {
+  it('Default node fillcolor is seleced from the fillcolor picker in the node format drawer', function() {
     localStorage.setItem('engine', 'circo');
     cy.startCleanApplication();
 
@@ -1213,7 +1213,7 @@ describe('Insertion of nodes into the graph', function() {
       let expectedFillColor;
       let expectedFillOpacity;
       if (positionName == 'left') {
-        expectedFillColor = 'transparent';
+        expectedFillColor = 'none';
         expectedFillOpacity = null;
       } else {
         const {x, y} = positions['topRight'];
@@ -1239,7 +1239,7 @@ describe('Insertion of nodes into the graph', function() {
         expect(ellipse).to.not.have.attr('stroke-opacity');
         expect(ellipse).to.have.attr('stroke', 'black');
         const actualFillColor = ellipse.attr('fill').replace('#', '');
-        if (expectedFillColor == 'transparent') {
+        if (expectedFillColor == 'none') {
           expect(actualFillColor).to.eq(expectedFillColor);
         } else {
           checkColor(actualFillColor, expectedFillColor, colorTolerance, 'fill');
