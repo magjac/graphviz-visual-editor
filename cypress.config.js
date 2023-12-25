58cded6013675fe6c13f75a240f1d9ee878c214e
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-25 15:42:16 +0100

    remove deprecated cypress/plugins/index.js
    See
    https://docs.cypress.io/guides/tooling/plugins-guide#Using-a-plugin.
    
    This is a cleanup after 240e6e4e4a77f5e189b2bec180282bb47f471fe2.

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,10 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   video: true,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      require('@cypress/code-coverage/task')(on, config);
+
+      return config;
     },
     excludeSpecPattern: '*~',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
