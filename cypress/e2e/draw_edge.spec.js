004f495d15082e7658409f3b5d0ce62d08432d86
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-02-13 19:44:25 +0100

    add Cypress test of drawing an undirected edge

--- cypress/e2e/draw_edge.spec.js
+++ cypress/e2e/draw_edge.spec.js
@@ -73,6 +73,39 @@ describe('Draw edges in the graph', function() {
     cy.edges().should('have.length', 1);
   })
 
+  it('Draws an edge from a node to another node in an undirected graph', function() {
+    cy.startApplication();
+    cy.clearAndRenderDotSource('graph {Alice Bob}');
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.edge(1).should('not.exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+
+    cy.nodes().should('have.length', 2);
+    cy.edges().should('have.length', 0);
+
+    cy.node(1).trigger('contextmenu');
+    cy.edge(1).should('exist');
+    cy.node(2).trigger('mousemove');
+    cy.node(2).dblclick();
+    cy.textEditorGutterCellWithError().should('not.exist');
+    cy.waitForTransition();
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.edge(1).shouldHaveName('Alice--Bob');
+
+    cy.nodes().should('have.length', 2);
+    cy.edges().should('have.length', 1);
+  })
+
   it('Draws edges in both directions between two nodes', function() {
     cy.startApplication();
     cy.clearAndRenderDotSource('digraph {Alice Bob}');
