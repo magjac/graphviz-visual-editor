741b72d95f22ba4b62cb619dec0f5a1897cdb96c
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-08-17 13:33:13 +0200

    generate-versions.py: fix syntax warning about invalid escape sequences '\[' and '\]'

--- bin/generate-versions.py
+++ bin/generate-versions.py
@@ -12,7 +12,7 @@ def main(args: List[str]) -> int: # pylint: disable=missing-function-docstring
   with open(args[1]) as fp:
     versions = {}
     for  line in fp:
-      mo = re.match("## \[([0-9][^\]]*)] [-–] (.*)$", line)
+      mo = re.match("## \\[([0-9][^\\]]*)] [-–] (.*)$", line)
       if mo:
         version = mo.group(1)
         release_date = mo.group(2)
