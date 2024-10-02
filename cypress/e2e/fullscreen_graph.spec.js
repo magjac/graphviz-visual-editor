a705d453d7ec1542925737821e2609a9ddcfdf06
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-10-02 21:03:53 +0200

    add test of fullscren enablee through 'f' key after disable with button

--- cypress/e2e/fullscreen_graph.spec.js
+++ cypress/e2e/fullscreen_graph.spec.js
@@ -99,4 +99,43 @@ describe('Show graph only mode', function () {
     cy.toolbar().should('exist');
     cy.canvas().invoke('width').should('be.lt', viewportWidth / 2)
   });
+
+  it('Shows the graph only when the \'f\' key is pressed after a previous fullscreen has been disabled by clicking the open in full button', function () {
+    cy.startCleanApplication();
+    cy.clearAndRenderDotSource('digraph {Alice -> Bob}');
+
+    cy.node(1).should('exist');
+    cy.node(2).should('exist');
+    cy.edge(1).should('exist');
+
+    cy.node(1).shouldHaveName('Alice');
+    cy.node(2).shouldHaveName('Bob');
+    cy.edge(1).shouldHaveName('Alice->Bob');
+
+    cy.nodes().should('have.length', 2);
+    cy.edges().should('have.length', 1);
+
+    cy.textEditorWrapper().should('be.visible');
+    cy.toolbar().should('exist');
+    cy.canvas().invoke('width').should('be.lt', viewportWidth / 2)
+
+    cy.canvas().click();
+    cy.get('body').type('f');
+
+    cy.textEditorWrapper().should('not.be.visible');
+    cy.toolbar().should('not.exist');
+    cy.canvas().invoke('width').should('be.eq', viewportWidth)
+
+    cy.fullscreenButton().click();
+
+    cy.textEditorWrapper().should('be.visible');
+    cy.toolbar().should('exist');
+    cy.canvas().invoke('width').should('be.lt', viewportWidth / 2)
+
+    cy.get('body').type('f');
+
+    cy.textEditorWrapper().should('not.be.visible');
+    cy.toolbar().should('not.exist');
+    cy.canvas().invoke('width').should('be.eq', viewportWidth)
+  });
 });
