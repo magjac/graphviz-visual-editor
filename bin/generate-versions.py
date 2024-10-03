f9f8cb94ade349b00c0505a3b911250edd502abd
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-10-03 20:29:41 +0200

    generate-versions.py: open file using UTF-8 encoding
    This avoids the following error on Windows:
    
    ```
    Traceback (most recent call last):
      File "C:\Users\magjac\graphviz-visual-editor\bin\generate-versions.py", line 26, in <module>
        sys.exit(main(sys.argv))
                 ^^^^^^^^^^^^^^
      File "C:\Users\magjac\graphviz-visual-editor\bin\generate-versions.py", line 14, in main
        for  line in fp:
      File "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.11_3.11.2544.0_x64__qbz5n2kfra8p0\Lib\encodings\cp1252.py", line 23, in decode
        return codecs.charmap_decode(input,self.errors,decoding_table)[0]
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    UnicodeDecodeError: 'charmap' codec can't decode byte 0x9d in position 977: character maps to <undefined>
    ```

--- bin/generate-versions.py
+++ bin/generate-versions.py
@@ -9,7 +9,7 @@ from typing import List
 
 def main(args: List[str]) -> int: # pylint: disable=missing-function-docstring
 
-  with open(args[1]) as fp:
+  with open(args[1], encoding="utf-8") as fp:
     versions = {}
     for line in fp:
       mo = re.match("## \\[([0-9][^\\]]*)] [-–] (.*)$", line)
